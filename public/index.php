c1fef0cb509793c1c4f507dfa3f362578d2f4513
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-02-10 10:37:40 +0100

    SDK dey ment

--- public/index.php
+++ public/index.php
@@ -2,3 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\PaymentProcessor;
+
+$data = [
+    "amount"=> 3500,
+    "currency"=> "NGN",
+    "redirectUrl"=> "http://localhost:8080/test-credo/welcome/",
+    "paymentOptions"=> "CARD",
+    "transRef" => "zubs1234",
+    "customerEmail"=> "zubairidrisaweda@yahoo.com",
+    "customerName"=> "Zubby's Babe",
+    "customerPhoneNo"=> "08024337127"
+];
+
+$processor = new PaymentProcessor();
+var_dump($processor->initTransaction($data));
